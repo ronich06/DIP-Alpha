3b5bc9a535cba030c726224e14dec458c4d6adab
Roni <ronyca99@hotmail.com>
2021-03-20 17:09:41 -0600

    IVaccination.h

---
+++ Ejemplo DIP2/IVaccination.h
@@ -0,0 +1,14 @@
+//
+// Created by Roni on 3/20/2021.
+//
+
+#ifndef EJEMPLO_DIP2_IVACCINATION_H
+#define EJEMPLO_DIP2_IVACCINATION_H
+#include "iostream"
+
+using namespace std;
+class IVacciantion{
+public:
+    virtual string vaccineProcess() = 0;
+};
+#endif //EJEMPLO_DIP2_IVACCINATION_H
