0bc7728fcdb7330bde84fb901da344bcf05b3e0d
Roni <ronyca99@hotmail.com>
2021-03-20 16:49:50 -0600

    Hospital.cpp y .h

---
+++ Ejemplo DIP2/Hospital.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Roni on 3/20/2021.
+//
+
+#include "Hospital.h"
---
+++ Ejemplo DIP2/Hospital.h
@@ -0,0 +1,14 @@
+//
+// Created by Roni on 3/20/2021.
+//
+
+#ifndef EJEMPLO_DIP2_HOSPITAL_H
+#define EJEMPLO_DIP2_HOSPITAL_H
+
+
+class Hospital {
+
+};
+
+
+#endif //EJEMPLO_DIP2_HOSPITAL_H
