513d5cac4ff260943f0faca5945d99da77791641
JordanJazz2430 <80367259+JordanJazz@users.noreply.github.com>
2021-03-20 17:52:28 -0600

    clase moderna

---
+++ Ejemplo DIP2/Moderna.cpp
@@ -0,0 +1,5 @@
+
+#include "Moderna.h"
+string Moderna::vaccineProcess() {
+    return "Moderna vaccine applied";
+}
---
+++ Ejemplo DIP2/Moderna.h
@@ -0,0 +1,19 @@
+//
+// Created by Roni on 3/20/2021.
+//
+
+#ifndef EJEMPLO_DIP2_MODERNA_H
+#define EJEMPLO_DIP2_MODERNA_H
+#include "iostream"
+#include "IVaccination.h"
+
+using namespace std;
+
+class Moderna : public IVaccination
+{
+public:
+    string vaccineProcess() override;
+};
+
+
+#endif //EJEMPLO_DIP2_MODERNA_H
