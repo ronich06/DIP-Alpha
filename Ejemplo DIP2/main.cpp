5c093bd6f8a38fe062f1d753b5fd5f134ad805f3
JordanJazz <80367259+JordanJazz@users.noreply.github.com>
2021-03-24 14:53:59 -0600

    arreeglo main

--- Ejemplo DIP2/main.cpp
+++ Ejemplo DIP2/main.cpp
@@ -7,14 +7,12 @@
 int main() {
     //
     Hospital* hospital= new Hospital("San Juan de Dios");
-    Moderna* _vacunaModerna;
-    Johnson* _vacunaJohnsons;
-    PfizerBioNTech* _vacunaPfizer;
+    Moderna* _vacunaModerna= new Moderna();
+    Johnson* _vacunaJohnsons= new Johnson() ;
+    PfizerBioNTech* _vacunaPfizer= new PfizerBioNTech();
 
+   cout<< hospital->vaccinate(_vacunaModerna);
 
-   string t= hospital->vaccinate(_vacunaModerna);
-
-    cout<<"Holadvdsvdfsfsdfwdfdfdwfddfgefgs";
 
     return 0;
 }
